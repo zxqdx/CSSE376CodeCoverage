aa690ebd5e5fb5a4f032839dd003120206dfc39e
zxqdx <acfun.zxqdx@gmail.com>
2015-04-14 19:20:55 -0400

    Update the test method to find bugs in bookWithDoubleMiles.

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -100,6 +100,22 @@ public class UserTest
 		Assert.assertEquals(5000, target.bonusFrequentFlierMiles, 0.01);
 	}
 
+	@Test
+	public void TestThatDiscountInitializes()
+	{
+		Discount target = new Discount(0.01, 1);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+		assertEquals(1024.65,this.target.Price(), 0.01);
+	}
+	
+	@Test
+	public void TestDoubleMilesWhenMilageDoesNotExceed()
+	{
+		Flight flight = new Flight(StartDate, EndDate, 1200);
+		target.bookWithDoubleMiles(new Booking[]{flight});
+		Assert.assertEquals(1200, target.bonusFrequentFlierMiles, 0.01);
+	}
 	
 	@After
 	public void TearDown()
